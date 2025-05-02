dd44fedfa063531f5f24c40c78730e957da64abf
CipherSentry <105765079+CipherSentry@users.noreply.github.com>
2025-05-02 15:20:08 -0400

    Completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+//.replaceAll(/[^A-Za-z0-9]/g,"")
+    let cleanString = string.replaceAll(/[^A-Za-z0-9]/g,"").toLowerCase();
+    let reversedString = cleanString.split("").reverse().join("");
+    let result = cleanString == reversedString;
+    
+    return result;
 };
 
 // Do not edit below this line
