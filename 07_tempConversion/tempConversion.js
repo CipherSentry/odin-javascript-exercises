3dfb36d88fa066e2ddfec0d2f99e6aa8967e017b
CipherSentry <105765079+CipherSentry@users.noreply.github.com>
2025-05-02 11:32:31 -0400

    Completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+// C = (F - 32) * 5/9
+// F  = (C * 9/5) + 32
+
+const convertToCelsius = function(fahrenheit) {
+  let celcius = parseFloat(((fahrenheit - 32) * (5/9)).toFixed(1))
+  return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fahrenheit = parseFloat(((celcius * (9/5)) + 32).toFixed(1))
+  return fahrenheit;
 };
 
 // Do not edit below this line
