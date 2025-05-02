85400151674a96c539134c556c2e99d71a958d11
CipherSentry <105765079+CipherSentry@users.noreply.github.com>
2025-05-02 12:14:05 -0400

    Completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,44 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+  let total = 0;
+	for (num of numArray) {
+    total += num;
+  };
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(numArray) {
+  let total = 1;
 
+  for (num of numArray) {
+    total = total * num;
+  };
+
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return num ** power;
 };
 
-const factorial = function() {
+const factorial = function(num) {
+
+  let factorial = 1;
+
+  if (num == 0) return 1;
+
+  for (i = 1; i <= num; i++) {
+    factorial = factorial * i;
+  };
+
+  return factorial;
 	
 };
 
