5bf95942fd91c9e27772781c0dce03feac260792
CipherSentry <105765079+CipherSentry@users.noreply.github.com>
2025-05-02 00:05:01 -0400

    Completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const input = string;
+    let output = "";
 
+    for (i = input.length - 1; i >= 0; i--) {
+        output += input.at(i);
+    }
+
+    return output;
 };
 
 // Do not edit below this line
