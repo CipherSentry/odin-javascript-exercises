1c1eeed99d84e3f02d61f09a815546adf1ba23c7
CipherSentry <105765079+CipherSentry@users.noreply.github.com>
2025-05-02 21:20:43 -0400

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    const date = new Date();
+    const year = date.getFullYear();
+
+    let oldest ={
+        name: "",
+        yearOfBirth: 0,
+        yearOfDeath: 0
+    };
+    let oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+
+    for (person of people) {
+       let age = 0;
+
+       if (person.yearOfDeath) {
+        age = person.yearOfDeath - person.yearOfBirth;
+       }else {
+        age = year - person.yearOfBirth;
+       }
+       
+       if (age > oldestAge) {
+        oldestAge = age;
+        oldest = person;
+       }
+
+    };
+
+    return oldest;
 };
 
 // Do not edit below this line
